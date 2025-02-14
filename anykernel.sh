cd7911dd49291294bbecedba45cfdc2c1efaba65
YuzakiKokuban <heibanbaize@gmail.com>
2025-02-14 21:54:18 +0800

    edit anykernel, change tools to aarch64

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=kalama kernel with KernelSU by Yufz
+kernel.string=S23-Knox-Disabled-Kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
