fcb8c38dda1660f08dc2f376e7b0e4a107be3d56
YuzakiKokuban <heibanbaize@gmail.com>
2025-02-14 21:56:34 +0800

    edit anykerne.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=S23-Knox-Disabled-Kernel
+kernel.string=S23-Knox-Disabled-Kernel-Kokuban
 do.devicecheck=1
 do.modules=0
 do.systemless=1
